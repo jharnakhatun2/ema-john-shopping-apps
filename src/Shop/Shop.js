49169cbed69f74948ce15e0d84deb0be35473822
Jharna203 <jharnakhatun203@gmail.com>
2022-09-27 16:35:47 +0600

    added product in local storage

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb } from '../utilities/fakedb';
 import './Shop.css';
 
 const Shop = () => {
@@ -18,6 +19,7 @@ const Shop = () => {
         //cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
